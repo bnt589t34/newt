e0f6f66b98f496840c2c0f1f216e77c2336e780a
bnt589t34 <arcangelcharlesmarmol@gmail.com>
2026-02-10 09:36:40 +0800

    Add files via upload

---
+++ app/src/main/java/com/zlt/minimal/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.zlt.minimal
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
